85afdb9a44530625d20b39f9bad24b037021eff8
Abdi Deresa <Abdishegute@gmail.com>
2025-02-15 09:19:20 -0800

    fetch and display blog posts from API in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
 import { get } from "./util/http";
+import { useEffect, useState } from "react";
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import fetchingImg from "./assets/data-fetching.png";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
 
 function App() {
-  get("https://jsonplaceholder.typicode.com/todos/1");
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
 
-  return <h1>Data Fetching!</h1>;
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+      setFetchedPosts(blogPosts);
+    }
+    fetchPosts();
+  }, []);
+
+  let content;
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="Fetching data..." />
+      {content}
+    </main>
+  );
 }
 
 export default App;
