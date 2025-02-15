3bcb562d711850e978b64a7d339fd78f0e792794
Abdi Deresa <Abdishegute@gmail.com>
2025-02-15 10:01:40 -0800

    add error handling and loading state to fetch posts in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { get } from "./util/http";
 import { useEffect, useState } from "react";
 import BlogPosts, { BlogPost } from "./components/BlogPosts";
 import fetchingImg from "./assets/data-fetching.png";
+import { ReactNode } from "react";
+import ErrorMessage from "./components/ErrorMessage";
 
 type RawDataBlogPost = {
   id: number;
@@ -12,27 +14,42 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get(
-        "https://jsonplaceholder.typicode.com/posts"
-      )) as RawDataBlogPost[];
+      setIsFetching(true);
+      setError(undefined);
 
-      const blogPosts: BlogPost[] = data.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
-        };
-      });
-      setFetchedPosts(blogPosts);
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts"
+        )) as RawDataBlogPost[];
+
+        const blogPosts: BlogPost[] = data.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+        setFetchedPosts(blogPosts);
+      } catch (err) {
+        setError("Failed to fetch posts.");
+      } finally {
+        setIsFetching(false);
+      }
     }
     fetchPosts();
   }, []);
 
-  let content;
-  if (fetchedPosts) {
+  let content: ReactNode;
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  } else if (isFetching) {
+    content = <p id="loading-fallback">Fetching posts....</p>;
+  } else if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />;
   }
 
